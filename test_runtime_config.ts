c640bad5e71be0c374a928c2d38565d7dd27cbef
Daniel Moore <dan@mooresolutions.io>
2026-05-19 11:19:42 -0500

    Clean up repository by removing outdated and unused test scripts.

--- test_runtime_config.ts
+++
@@ -1,4 +0,0 @@
-import { useRuntimeConfig } from '#imports'
-
-const runtimeConfig = useRuntimeConfig()
-console.log('runtimeConfig:', runtimeConfig)
--- test_templates.ts
+++
@@ -1,9 +0,0 @@
-import { getVerificationEmail, getPasswordResetEmail } from './server/utils/email-templates';
-
-const verif = getVerificationEmail('https://mootasks.dev/verify/123');
-console.log('Verification Email:', verif.subject);
-console.log('Verification HTML:', verif.html);
-
-const reset = getPasswordResetEmail('https://mootasks.dev/reset/456');
-console.log('Reset Email:', reset.subject);
-console.log('Reset HTML:', reset.html);
